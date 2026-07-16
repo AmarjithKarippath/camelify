956ea59ac6773bd92b949838449df0781879de5f
Amarjith Sudhakar K <amarjithkarippath@gmail.com>
2026-07-16 07:12:33 +0100

    removed few

--- tally-mvp/frappe_app/tally_mvp/tally_mvp/boot.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def boot_session(bootinfo):
-    bootinfo.tally_mvp = {
-        "version": "0.1.0",
-        "default_workspace": "Tally Owner",
-        "dashboard_route": "/app/tally-dashboard",
-    }
